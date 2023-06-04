bb50c896d50521c6402ede24f146bcdf2b1c7c5a
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-06-04 18:05:56 +0300

    Included Password Hashing

--- models/users.js
+++ models/users.js
@@ -1,6 +1,7 @@
 const isEmail = require('validator/lib/isEmail');
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Create user schema
 const userSchema = new mongoose.Schema({
@@ -19,6 +20,15 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please enter a password'],
     minlength: [6, 'Minimum password length is 6 characters'],
   },
+});
+
+// Run a function before doc saved to MongoDB
+// eslint-disable-next-line func-names
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
 // Create a model from the created Schema above
